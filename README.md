5d83186e4284f671f1cc33944304a8266d488913
Ailln <kinggreenhall@gmail.com>
2020-10-21 06:43:07 +0800

    add tensorboard.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # xor-net
-使用两层神经网络解决异或问题
+
+使用两层神经网络解决异或问题，主要目的是在简单网络中，可以很方便的推导反向传播中的梯度。
+
+## 运行
+
+```bash
+pip install torch tensorboard
+
+python xor.py
+```
+
+## 可视化
+
+```bash
+tensorboard --logdir=runs
+
+# open http://localhost:6006
+```
+
+![](src/graph.png)
--- xor.py
+++ xor.py
@@ -1,8 +1,7 @@
-# ! pip install torch==1.6.0
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
 
 xor_data = [
     [[1, 1], 0],
@@ -11,27 +10,32 @@ xor_data = [
     [[0, 0], 0]
 ]
 
+
 class XORNet(nn.Module):
     def __init__(self):
         super(XORNet, self).__init__()
-        self.w1 = nn.Parameter(torch.tensor([[0.1, -0.1], [-0.1, 0.1]]))
-        self.w2 = nn.Parameter(torch.tensor([[0.1, -0.1], [-0.1, 0.1]]))
-        self.b1 = nn.Parameter(torch.tensor([0.1, -0.1]))
-        self.b2 = nn.Parameter(torch.tensor([-0.1, 0.1]))
+        self.linear1 = nn.Linear(2, 2)
+        self.linear2 = nn.Linear(2, 2)
+        self.linear1.weight.data = torch.tensor([[0.1, -0.1], [-0.1, 0.1]])
+        self.linear2.weight.data = torch.tensor([[-0.1, 0.1], [0.1, -0.1]])
+        self.linear1.bias.data = torch.tensor([0.1, -0.1])
+        self.linear2.bias.data = torch.tensor([-0.1, 0.1])
 
     def forward(self, x):
-        x = F.linear(x, weight=self.w1, bias=self.b1)
-        x = torch.sigmoid(x)
-        x = F.linear(x, weight=self.w2, bias=self.b2)
-        x = torch.sigmoid(x)
-        return x
+        out1 = self.linear1(x)
+        out1 = torch.sigmoid(out1)
+        out2 = self.linear2(out1)
+        out2 = torch.sigmoid(out2)
+        return out2
 
-epoch = 1000
+
+epoch = 600
 log_interval = 100
 learning_rate = 1
 
 model = XORNet()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+writer = SummaryWriter()
 
 for e_index in range(1, epoch+1):
     for index, data in enumerate(xor_data):
@@ -48,9 +52,14 @@ for e_index in range(1, epoch+1):
         optimizer.step()
 
         if e_index == 1 and index == 0:
+            writer.add_graph(model, _input)
+
             print(f"\nweight(after): {model.state_dict()}]\n")
             for name, parms in model.named_parameters():
                 print(f"{name} grad ({parms.requires_grad}): {parms.grad}")
 
         if not e_index % log_interval:
-            print(f"epoch: {e_index} input: {_input.data} output: {output.data}, target: {target.data} loss: {loss.data}")
+            print(
+                f"epoch: {e_index} input: {_input.data} output: {output.data}, target: {target.data} loss: {loss.data}")
+
+writer.close()
